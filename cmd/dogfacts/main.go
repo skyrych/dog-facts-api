0c6ba7f31b5259694c906f4a7158d6ba3b60c0a1
Serhii Kyrychenko <serhii.kyrychenko@wpengine.com>
2025-07-24 12:00:41 +0300

    main.go is almost over

--- cmd/dogfacts/main.go
+++ cmd/dogfacts/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	app "github.com/skyrych/dog-facts-api/internal/app/dogfacts"
+)
+
+func main() {
+	needyRandomFact := app.NewFactServer()
+	err := app.StarServer(":8080", needyRandomFact)
+	if err != nil {
+		fmt.Println("Failed to run a server")
+	}
 }
