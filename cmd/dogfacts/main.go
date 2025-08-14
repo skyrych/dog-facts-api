746bf265daf2015b2b5cf09560d07c4f913b8404
Serhii Kyrychenko <serhii.kyrychenko@wpengine.com>
2025-08-14 10:30:53 +0300

    Added logging

--- cmd/dogfacts/main.go
+++ cmd/dogfacts/main.go
@@ -2,15 +2,37 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-	app "github.com/skyrych/dog-facts-api/internal/app/dogfacts" // <--- ENSURE THIS LINE IS CORRECT!
+	app "github.com/skyrych/dog-facts-api/internal/app/dogfacts"
 )
 
 func main() {
+
 	needyRandomFact := app.NewFactServer()
 	err := app.StartServer(":80", needyRandomFact)
 	if err != nil {
 		log.Fatalf("Server failed to start: %v", err)
+	}
+
+	_, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+	case s := <-sigs:
+		fmt.Printf("Received the signal %s: ", s)
+		cancel()
+	case <-time.After(10 * time.Second):
+		fmt.Println("Graceful shutdown initiated due to timeout.")
+		cancel()
 	}
 }
--- internal/app/dogfacts/server.go
+++ internal/app/dogfacts/server.go
@@ -2,14 +2,17 @@ package dogfacts
 
 import (
 	"fmt"
+	"log/slog"
 	"math/rand"
 	"net/http"
+	"os"
 	"time"
 )
 
 type FactServer struct {
-	Facts []string
-	Rand  *rand.Rand
+	Facts  []string
+	Rand   *rand.Rand
+	Logged *slog.Logger
 }
 
 func NewFactServer() *FactServer {
@@ -19,16 +22,23 @@ func NewFactServer() *FactServer {
 			"Needy has friends and enemies among the dogs.", "Needy naps throughout the day.", "Needy becomes anxious and restless when he wants to pee.",
 			"Needy loves sweets.", "Needy hunts cats, mice and frogs", "Needy loves going out with his humans.",
 			"Needy prefers local outings to coffee shops over long-distance travel.", "Needy has a main and a spare human."},
-		Rand: rand.New(rand.NewSource(time.Now().UnixNano())),
+		Rand:   rand.New(rand.NewSource(time.Now().UnixNano())),
+		Logged: slog.New(slog.NewJSONHandler(os.Stdout, nil)),
 	}
 }
 
 func StartServer(port string, fs *FactServer) error {
+	fs.Logged.Info("Starting Dog Facts API server", "port", port)
 	http.HandleFunc("/facts", fs.factsHandler)
 	return http.ListenAndServe(port, nil)
 }
 
 func (fs *FactServer) factsHandler(res http.ResponseWriter, req *http.Request) {
+	fs.Logged.Info("Received a request:", "Method", req.Method, "Path", req.URL.Path)
+	if len(fs.Facts) == 0 {
+		http.Error(res, "Looks like we are out of facts", 503)
+		return
+	}
 	num := fs.Rand.Intn(len(fs.Facts))
 	fmt.Fprint(res, fs.Facts[num], "\n")
 }
